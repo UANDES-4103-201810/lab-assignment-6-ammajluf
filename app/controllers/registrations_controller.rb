1fdae4dee9100a35b1bb581cc5ae8bc4186224e9
fmoreira28 <fjmoreira@miuandes.cl>
2018-04-17 14:48:15 -0300

    registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,22 @@ class RegistrationsController < ApplicationController
 	end
 
 	def create
-	    #complete this method
+		@user = User.new(registrations_params)
+			if @user.save
+				flash[:notice] = "User registered"
+				redirect_to root_path
+			else
+				flash[:notice] = "Error in registration"
+				redirect_to sign_in_path
+			end
+
 	end
+
+	private
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def registrations_params
+		params.require(:registrations).permit(:name, :last_name, :email, :password, :phone)
+	end
+
 end
